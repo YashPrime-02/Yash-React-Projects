0f5224b158af3894069b68949b68ab46ad4e3329
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2024-03-07 18:02:53 +0530

    commit  done  right

---
+++ 01basicreact/01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+
+
+  return (
+    <h1> Chai aur react with Vite | Yash Mishra</h1>
+  )
+}
+
+export default App
---
+++ 01basicreact/src/App.js
@@ -0,0 +1,9 @@
+
+
+function App() {
+  return (
+   <h1> Chai Aur React</h1>
+  );
+}
+
+export default App;
