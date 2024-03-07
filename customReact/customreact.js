c21a353b6cad462b4c68226906151de0b0d6a72b
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2024-03-07 18:28:57 +0530

    created customRender

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,5 +1,14 @@
+function customRender(reactElement, container)
+{
+ const domElement= document.createElement('reactElement.type')
+ domElement.innerHTML = reactElement.children
+ domElement.setAttribute('href',reactElement.props.href)
+ domElement.setAttribute('target',reactElement.props.target)
+ container.appendChild(domElement)
+   
+}
 const reactElement = {
-    type: "div",
+    type: 'a',
     props: {
       href: 'https://google.com',
       target: '_blank',
@@ -8,3 +17,5 @@ const reactElement = {
     children: 'Click me to visit google'
 }
 const mainContainer=document.querySelector('#root')
+
+customRender(reactElement, mainContainer)
