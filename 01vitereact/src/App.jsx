eedd94862bdbe9c82329563b47d5ea99febcceb2
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2024-03-08 02:06:46 +0530

    changes

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,11 @@
 
 import Chai from "./chai"
 function App() {
+const username="Yash Mishra"
 return (
   <>
    <Chai/>
-   <h2>Yash Ka React Project </h2>
+   <h2>{username} Ka React Project </h2>  
    <p>It Works</p>
    </>
   )
