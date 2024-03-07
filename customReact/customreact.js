458a5895f2a0896a9afe6693727c16ea02127eed
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2024-03-08 01:09:24 +0530

    Made some shitty changes

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,11 +1,13 @@
 function customRender(reactElement, container)
 {
- const domElement= document.createElement('reactElement.type')
+  /*
+ const domElement= document.createElement(reactElement.type)
  domElement.innerHTML = reactElement.children
  domElement.setAttribute('href',reactElement.props.href)
  domElement.setAttribute('target',reactElement.props.target)
  container.appendChild(domElement)
-   
+   */ // Was Done as in this we have to add individual attributes 
+
 }
 const reactElement = {
     type: 'a',
