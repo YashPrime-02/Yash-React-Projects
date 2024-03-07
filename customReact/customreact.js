4519c46ab285c53b30e10da9db501c24e885ef5e
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2024-03-07 18:20:27 +0530

    created custom react element

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,1 +1,10 @@
+const reactElement = {
+    type: "div",
+    props: {
+      href: 'https://google.com',
+      target: '_blank',
+      
+    },
+    children: 'Click me to visit google'
+}
 const mainContainer=document.querySelector('#root')
