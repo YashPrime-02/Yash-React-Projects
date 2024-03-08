6ef935df1e6d6936728307287c75b7a42fd6ed13
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2024-03-08 20:17:57 +0530

    Made A Counter Application

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+
+function App()
+ {
+ let[counter,setCounter]= useState(15) //it propogates state changes to UI components , bracket me default value hota
+//let counter=15
+const addValue =() => {
+counter=counter+1
+setCounter(counter) //let use kiya coz update counter kar rahe value
+//console.log(counter)
+}
+const removeValue =() => 
+{
+counter=counter-1
+setCounter(counter)
+//console.log(counter)
+}
+
+  return (
+    <>
+      <h1> <strong> Number Counter App </strong> </h1>
+      <h2>(ADD OR REMOVE VALUES)</h2>
+      <h2>Current Counter Value: {counter}</h2>
+      <button onClick={addValue}> Add Value</button>
+      <br/>
+      <button onClick={removeValue}> Remove Value</button>
+    </>
+  )
+}
+
+export default App
