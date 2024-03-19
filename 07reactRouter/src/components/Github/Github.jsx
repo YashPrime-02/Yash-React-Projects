3184e18c696b67c0e09289cd3e543c776912a456
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2024-03-19 17:29:49 +0530

    Router Project Done

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect ,useState} from 'react'
+
+import { useLoaderData } from 'react-router-dom'
+function Github() 
+{
+/*const [data, setData]= useState([])  
+  useEffect(() =>
+  {
+  fetch('https://api.github.com/users/YashPrime-02')
+  .then(response => response.json())
+  .then(data=> {
+    console.log(data)
+    setData(data)
+  })
+
+},[])    // Method 1 
+*/ 
+  const data=useLoaderData()
+
+  return (
+    <div className='text-center m-4  text-white p-4 text-3xl bg-gray-500' >
+        Github Followers : {data.followers}
+        <img src={data.avatar_url} alt='GitHub Photo' width={350}/>
+        
+         </div>
+  )
+}
+
+export default Github
+export const githubInfoLoader= async() =>
+{
+
+    const response = await fetch('https://api.github.com/users/YashPrime-02')
+    return response.json()
+}
