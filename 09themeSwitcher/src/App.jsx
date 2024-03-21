f88726269d5500a014ee2d5a4c152253f91b69cc
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2024-03-21 14:49:09 +0530

    9th Project Started

---
+++ 09themeSwitcher/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react"
+
+
+function App() { //Method 2 to create Context
+  
+  return (
+    <>
+<div className="flex flex-wrap min-h-screen items-center">
+    <div className="w-full">
+       <div className="w-full max-w-sm mx-auto 
+           flex justify-end mb-4">
+          { /*Theme Buttons*/}
+         </div>
+            <div className="w-full max-w-sm mx-auto">
+              {/*Card*/}  
+            </div>
+      </div>
+</div>
+    </>
+  )
+}
+
+export default App
