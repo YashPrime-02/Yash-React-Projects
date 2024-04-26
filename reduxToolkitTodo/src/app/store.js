4831cf066db24463c7bf4dfbb27f20383efe6e39
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2024-04-27 02:28:58 +0530

    Todo done with redux

--- reduxToolkitTodo/src/app/store.js
+++ reduxToolkitTodo/src/app/store.js
@@ -2,4 +2,6 @@ import {configureStore} from '@reduxjs/toolkit';
 //ye core Redux se aaya hai, store banane ke liye import kara hai, 
 import todoReducer from '../features/todo/todoSlice';
 
-export const store= configureStore({})
+export const store= configureStore({
+    reducer:todoReducer
+})
