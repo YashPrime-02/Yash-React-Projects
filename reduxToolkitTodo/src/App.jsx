397e675120ec50e19e93e2d61ec5804c464dff45
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2024-04-27 01:53:33 +0530

    almost final

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
-
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 
 function App() {
   return (
 <>
 <h1> Redux ToolKit</h1>
+<AddTodo/>
+<Todos/>
+
 </>
   )
 }
--- reduxToolkitTodo/src/components/Todos.jsx
+++ reduxToolkitTodo/src/components/Todos.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { removeTodo } from '../features/todo/todoSlice'
 
 function Todos() {
-  return (
-    <div>Todos</div>
-  )
+    const todos = useSelector(state => state.todos)
+    const dispatch = useDispatch()
+    return (
+        <>
+        <div>Todos</div>
+            {todos.map(todo => (
+            <li key={todo.id}>
+                {todo.text}
+                <button onClick={() => dispatch(removeTodo(todo.id))}>
+                    X </button>
+            </li>    
+            ))}
+        </>
+    )
+ 
 }
 
 export default Todos
