5902594b24a87a2f1af085605279009398242a29
Yash Mishra <108348585+YashPrime-02@users.noreply.github.com>
2024-05-06 03:32:04 +0530

    Environment Variables (ENV) Done

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+   <>
+   <h1 >  A Blog APP with APPWRITE </h1>
+   
+   </>
+  )
+}
+
+export default App
